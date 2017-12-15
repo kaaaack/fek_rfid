50cbccb878d7e81ad5ef0c2ed062c8a9ef93902a
langmaack <langmaack@Winz213.local.schmelz.com>
2017-12-15 10:45:59 +0100

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+        <link href="CSS/style.css" rel="stylesheet">
+        <meta charset="UTF-8">
+        <title>FEK RFID OVM</title>
+    </head>
+    <body>
+        <h1 id="title">RFID Erfassung</h1>
+        <div id="border"></div>
+        <ul id="navi">
+        <li class="active"><a href="index.php" title="Home">Home</a></li>
+        <li><a href="#" title="Über Mich">Login</a></li>
+        <li><a href="#" title="Registrieren">Registrieren</a></li>
+        <li><a href="user.php" title="Benutzer">Benutzer-Datenbank</a></li>
+        <li><a href="#" title="Benutzer">RFID-Datenbank</a></li>
+        </ul>
+        <div id="padding"
+             <h2>Startseite</h2>
+            <form action="include/formular.php" method="get">
+            <p>Gib deinen Namen ein: <input type="text" name="benutzername" /></p>
+            <input type="submit" value="absenden" />
+            </form>
+        </div>
+    </body>
+</html>
